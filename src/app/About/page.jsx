1ec77d9584a89e54bb4a6fe46d8a440a4496ae50
RileyWong26 <rileywongwong@gmail.com>
2025-07-06 20:46:38 -0400

    Home page for navigation

---
+++ src/app/About/page.jsx
@@ -0,0 +1,10 @@
+import Header from "../header"
+
+export default function About(){
+    return (
+        <div className = "bg-background1 h-screen dark:bg-background1dark transition-colors duration-1000 ease-in-out">
+            <Header />
+            About me!!
+        </div>
+    )
+}
