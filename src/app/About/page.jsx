f17d547e765558f886c929e7f9ff9c51d7cd18b9
RileyWong26 <rileywongwong@gmail.com>
2025-07-12 19:56:00 -0400

    Changes for different screen sizes

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,34 +1,38 @@
 import Header from "@/app/header";
 import Image from "next/image";
+import HeaderButton from "../headerbutton";
 
-export default function About(){
+export default async function About(){
     return (
-        <div className = "bg-background1 w-screen h-screen transition-colors duration-1000 ease-in-out flex flex-row space-x-2 text-primary">
+        <div className = "bg-background1 w-screen min-h-screen transition-colors duration-1000 ease-in-out flex flex-row space-x-2 text-primary">
             <Header />
-            <div className="w-full h-full flex flex-row justify-evenly ">
-                <div className="w-4/5 h-full">
-                    <div className="w-4/5 h-full flex flex-col ml-auto animate-pullup">
-                        <h1 className="text-hightlight h-3/10 content-center font-bold text-3xl">About Me</h1>
-                        <h1 className=" h-2/10 leading-6">Hey, my name is Riley Wong, I am a developer passionate about creating robust software.  My love for creating started from trying to create both mods and games at age 10.  </h1>
-                        <h1 className=" h-2/10 leading-6">I am a team player who communicates effectively, faces problems critically and has the technical skills to tackle problems.  </h1>
-                        <h1 className=" h-2/10 leading-6">I am also passionate about music, and love to listen, play guitar and sing.</h1>
+            <HeaderButton />
+
+            <div className="w-full min-h-screen flex flex-col-reverse xl:flex-row justify-evenly py-10 space-y-5">
+                <div className="m-auto w-4/5 min-h-[50vh] xl:h-full">
+                    <div className="m-auto w-4/5 h-full flex flex-col space-y-8 xl:space-y-12 animate-pullup">
+                        <h1 className="text-hightlight h-2/10 xl:h-3/10 content-center font-bold  text-2xl xl:text-3xl">About Me</h1>
+                        <h1 className="text-xs xl:text-base leading-6">Hey, my name is Riley Wong, I am a developer passionate about creating robust software.  My love for creating started from trying to create both mods and games at age 10.  </h1>
+                        <h1 className="text-xs xl:text-base leading-6">I am a team player who communicates effectively, faces problems critically and has the technical skills to tackle problems.  </h1>
+                        <h1 className="text-xs xl:text-base leading-6">I am also passionate about music, and love to listen, play guitar and sing.</h1>
                     </div>
                 </div>
-                <div className="w-4/5  h-full animate-pullup">
-                    <div className="w-full h-full perspective-distant content-center">
-                        <div className="w-1/2 aspect-3/2 bg-black relative transform-3d m-auto rounded-2xl 
-                            transition-all duration-1000 ease-in-out rotate-x-5 rotate-y-20 scale-95 hover:-rotate-x-15 hover:-rotate-y-25 hover:scale-120 hover:-translate-y-5"> 
-                            <Image className="m-auto rounded-2xl h-full w-full"
+                <div className="m-auto w-4/5 min-h-[33vh] xl:h-[50vh] animate-pullup">
+                    <div className="w-full min-h-[33vh] perspective-distant content-center">
+                        <div className="w-3/4 h-8/10 xl:w-2/3 relative transform-3d m-auto rounded-2xl 
+                            transition-all duration-1000 ease-in-out xl:rotate-x-5 xl:rotate-y-20 scale-95 hover:-rotate-x-15 
+                            hover:-rotate-y-25 hover:scale-120 hover:-translate-y-5"> 
+                            <Image className="m-auto rounded-2xl h-full w-auto xl:w-full xl:h-auto"
                                 src="/Images/GDSxLOJAM1.png"
                                 width={0} height={0}
-                                objectFit="contain"
-                                layout="fill"   
+                                quality={100}
                                 alt="AboutMeImage"/>
                         </div>
-                        <h1 className=" w-3/4 m-auto text-center mt-5 text-sm"> Picture of me,(left most), and my friends winning the 2025 LoJam X GDS Game Jam</h1>
+                        <h1 className=" w-3/4 m-auto text-center mt-5 text-xs xl:text-sm"> Picture of me,(left most), and my friends winning the 2025 LoJam X GDS Game Jam</h1>
                     </div>
                 </div>
             </div>
+        
         </div>
     )
 }
