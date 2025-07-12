1059493d720a527af62877eff0fe6e0c6e07ab8c
RileyWong26 <rileywongwong@gmail.com>
2025-07-12 19:53:25 -0400

    Changes to update button

--- src/app/headerbutton.jsx
+++ src/app/headerbutton.jsx
@@ -4,46 +4,47 @@ import Image from "next/image";
  const HeaderButton = () =>{
     return(
         <div className="h-[10vh] aspect-square ml-auto -translate-x-1/2 bg-background2 rounded-4xl flex flex-col z-10
-            space-y-2 content-center group left-3/4 absolute xl:hidden transition-all duration-1000 ease-in-out active:bg-amber-200"
+            space-y-2 content-center group left-3/4 fixed xl:hidden transition-all duration-1000 ease-in-out"
                 id="HeaderButton"
                 onClick={() => {
                     document.getElementById("HeaderButton").classList.toggle("open");
                     const header = document.getElementById("Header");
                     if(header){
                         header.classList.toggle("-translate-y-full");
+                        header.classList.toggle("fixed");
                     };
 
                 }}>
-                <Image className="mx-auto mt-auto rounded-4xl group-[.open]:rotate-45 group-[.open]:translate-y-2.5 
+                <Image className="mx-auto mt-auto rounded-2xl group-[.open]:rotate-45 group-[.open]:translate-y-2.5 
                     transition-transform duration-500 ease-in-out dark:hidden "
-                    src={"LightMode/Line.webp"}
+                    src={"/LightMode/Line.webp"}
                     alt={"Line1"}
                     width={40}height={0}/>
                 <Image className="mx-auto rounded-4xl group-[.open]:opacity-0 transition-all duration-500 ease-in-out 
                     dark:hidden"
-                    src={"LightMode/Line.webp"}
+                    src={"/LightMode/Line.webp"}
                     alt={"Line2"}
                     width={40}height={0}/>
                 <Image className="mx-auto mb-auto rounded-4xl group-[.open]:-rotate-45 group-[.open]:-translate-y-2.5 
                     transition-transform duration-500 ease-in-out dark:hidden"
-                    src={"LightMode/Line.webp"}
+                    src={"/LightMode/Line.webp"}
                     alt={"Line3"}
                     width={40}height={0}/>
-                <Image className="mx-auto mt-auto rounded-4xl group-[.open]:rotate-45 group-[.open]:translate-y-2.5 
+                <Image className="mx-auto mt-auto rounded-xl group-[.open]:rotate-45 group-[.open]:translate-y-2.5 
                     transition-transform duration-500 ease-in-out hidden dark:inline"
-                    src={"DarkMode/Line.webp"}
+                    src={"/DarkMode/Line.webp"}
                     alt={"Line1"}
                     width={40}height={0}/>
-                <Image className="mx-auto rounded-4xl group-[.open]:opacity-0 transition-all 
+                <Image className="mx-auto rounded-xl group-[.open]:opacity-0 transition-all 
                     duration-500 ease-in-out hidden dark:inline"
-                    src={"DarkMode/Line.webp"}
+                    src={"/DarkMode/Line.webp"}
                     alt={"Line2"}
                     width={40}height={0}/>
-                <Image className="mx-auto mb-auto rounded-4xl group-[.open]:-rotate-45 hidden
+                <Image className="mx-auto mb-auto rounded-xl group-[.open]:-rotate-45 hidden
                     group-[.open]:-translate-y-2.5 transition-transform duration-500 ease-in-out dark:inline"
-                    src={"DarkMode/Line.webp"}
+                    src={"/DarkMode/Line.webp"}
                     alt={"Line3"}
-                    width={40}height={0}/>
+                    width={40} height={0}/>
         </div>
     )
 }
