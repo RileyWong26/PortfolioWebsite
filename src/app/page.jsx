1b7b8d8ff46444a8cd66ad04a2835584de8223c0
RileyWong26 <rileywongwong@gmail.com>
2025-07-11 21:11:53 -0400

    Adjustments to mobile devices

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,24 +2,27 @@ import Image from "next/image";
 import Header from "@/app/header";
 import Head from "next/head";
 import Three from "./three";
+import HeaderButton from "./headerbutton";
 
 export default async function Home() {
 
   return (
-    <div className = "bg-background1 w-screen h-screen font-Anonymous transition-colors duration-1000 ease-in-out flex flex-row space-x-2">
+    <div className = "bg-background1 w-screen min-h-screen font-Anonymous transition-colors duration-1000 ease-in-out flex flex-col xl:flex-row space-x-2">
       <Header />
-      <div className="w-full flex flex-row justify-evenly">
-        <div className="w-4/5 content-center animate-pullup ">
-            <h1 className="m-auto text-primary w-1/2 leading-15 font-bold text-3xl animate-glitchtext ">
+      <HeaderButton />
+
+      <div className="w-full max-w-screen min-h-screen flex flex-col xl:flex-row justify-evenly ">
+        <div className="w-full xl:w-4/5 max-w-screen h-screen content-center animate-pullup ">
+            <h1 className="m-auto text-primary w-3/4  leading-15 font-bold text-3xl animate-glitchtext ">
               Hi <br /> 
               I'm Riley <br/> 
               Software Engineer
             </h1>
-            <h1 className="ml-auto text-text2 w-3/4 text-lg ">
+            <h1 className="m-auto  text-text2 w-3/4 text-lg ">
               Computer science, 2029, Western University
             </h1>
         </div>
-        <div className="w-4/5 content-center">
+        <div className="w-4/5 max-w-screen content-center animate-pullup">
           <Three />
         </div>
       </div>
