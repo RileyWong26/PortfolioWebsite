7dde1cbb5d27aa0e134a68788abcbc7235cb1a93
RileyWong26 <rileywongwong@gmail.com>
2025-07-22 22:18:03 -0400

    Hero asset and responsive design

--- src/app/three.jsx
+++ src/app/three.jsx
@@ -4,33 +4,65 @@ import React, {useEffect} from 'react';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
-
 export default function Three(){
+    const resizeCanvas = () => {
+        // Reset the size of the canvas to fit the screen 
+        const heroSection = document.getElementById("HeroSection")
+        if(heroSection !== null){
+            return heroSection.clientWidth;
+        }
+        return 0;
+    }
 
     useEffect(() => {
+        
+
         // Instantiate scene
         const scene = new THREE.Scene();
         // Initalize and position camera
-        const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+        const fov = 75;
+        const aspect = 1;
+        const near = 0.1;
+        const far = 1000;
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
         camera.position.z = 5;
+        camera.position.y = 2;
         
         // Set Canvas
         const canvas = document.getElementById("ThreeContainer");
         const renderer = new THREE.WebGLRenderer({antialias: true, canvas, alpha: true});
-        renderer.setSize( 500, 500 );
 
+        // Set size
+        var canvasWidth = resizeCanvas();
+        renderer.setSize( canvasWidth, canvasWidth );
+
+        // Resize the canvas on window resizes
+        window.addEventListener("resize", () => {
+            canvasWidth = resizeCanvas();
+            renderer.setSize(canvasWidth, canvasWidth);
+        });
+
+        // Orbit
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.update();
 
         // Load Model
         const loader = new GLTFLoader();
-        var loadedmodel = null;
+        var person;
+        var mixer;
 
-        loader.load('/3D/Laptop.glb', function(model){
-            scene.add(model.scene);
-            model.scene.position.y = -1;
-            loadedmodel = model;
-            console.log(model);
+        loader.load('/HeroAsset/Coding.glb', function(model){
+            person = model.scene;
+            scene.add(person);
+            person.position.y = -3;
+            person.position.x = -1.5;
+            person.scale.set(.6, .6, .6);
+            person.rotation.set(0, 3 *Math.PI / 4, 0);
+            // Animation MIxer
+            mixer = new THREE.AnimationMixer(person);
+            const clips = model.animations;
+            mixer.clipAction(clips[0]).play();
+            mixer.update(deltaTime);
 
         }, undefined, function(error){
             console.log(error);
@@ -40,7 +72,12 @@ export default function Three(){
         // Light
         const lightColor = 0xFFFFFF;
         const lightIntensity = 5;
-        const light = new THREE.AmbientLight(lightColor, lightIntensity);
+        // const light = new THREE.AmbientLight(lightColor, lightIntensity);
+        const light = new THREE.DirectionalLight(lightColor, lightIntensity);
+        light.position.y = 5;
+        if(person !== undefined){
+            light.target = person;
+        }
         scene.add(light);
 
         // Raycast
@@ -60,31 +97,22 @@ export default function Three(){
             }
         }
         function mouseClick(event){
-            if (loadedmodel != null){
-                const pos = boundingRect(event);
-                mouse.x = (pos.x / canvas.width) * 2 - 1;
-                mouse.y = (pos.y / canvas.height) * -2 + 1;
-
-                raycaster.setFromCamera(mouse, camera);
-            
-                const intersects = raycaster.intersectObjects(loadedmodel.scene.children)[0];
-                if(intersects != undefined && intersects.object.name === "About_me"){
-                    console.log("About Me");
-                }
-                
-            }
+            return;
         }
-
+        
         // Animation Loop
         function animate() {
             controls.update();
             renderer.render(scene, camera);
+            if( mixer){
+                mixer.update(0.02);
+            }
         }
         renderer.setAnimationLoop(animate);
     }, []);
 
     return(
-        <canvas id="ThreeContainer">
+        <canvas id="ThreeContainer" className="overflow-hidden w-full">
 
         </canvas>
     )
