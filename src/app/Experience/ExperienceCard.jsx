7cb62bc5e95207485348cdf2225989b4caa9558a
RileyWong26 <rileywongwong@gmail.com>
2025-07-27 21:52:14 -0400

    Text changes for different screen sizes

--- src/app/Experience/ExperienceCard.jsx
+++ src/app/Experience/ExperienceCard.jsx
@@ -25,33 +25,33 @@ export default function ExperienceCard({title, data}){
                 (doc === null) ? null : doc.classList.toggle("animate-pullup2");
                 document.getElementById("ExperienceImageContainer").classList.toggle("animate-turn");
             }}>
-            <div className="h-2/10 w-full flex flex-row-reverse group">
-                <div className="w-1/30 aspect-square relative -translate-x-full 
-                    group-hover:translate-x-0 group-hover:-translate-y-1/3
+            <div className="w-full flex flex-row-reverse group relative">
+                <div className="absolute left-9/10  aspect-square 
+                    group-hover:translate-x-1/2 group-hover:-translate-y-1/6
                     transition-transform duration-500 ease-in-out">
                 
-                    <Image className="w-full h-auto hidden dark:block"
+                    <Image className="w-auto h-full lg:w-3/5 lg:h-auto hidden dark:block"
                         src="/DarkMode/ArrowUpDarkMode.webp"
-                        width={0} height={0}
+                        width={40} height={40}
                         quality={100}
                         alt=""/>
-                    <Image className=" w-full h-auto dark:hidden"
+                    <Image className="w-auto h-full lg:w-3/5 lg:h-auto dark:hidden"
                         src="/LightMode/ArrowUpLightMode.webp"
-                        width={0} height={0}
+                        width={40} height={40}
                         quality={100}
                         alt=""/>
                 </div>
-                <h1 className="w-full content-center text-base xl:text-xl">
+                <h1 className="w-full content-center text-xl ">
                     {title}
                 </h1>
             </div>
-            <h1 className="text-text2 h-1/10 text-sm xl:text-lg ">
+            <h1 className="text-text2 text-lg ">
                 {role}
             </h1>
-            <h1 className="text-xs xl:text-base h-5/10">
+            <h1 className="text-base h-5/10 truncate">
                 {description}
             </h1>
-            <h1 className="text-right h-1/10 text-xs xl:text-base text-text2">
+            <h1 className="text-right h-1/10 text-base text-text2 text-balanced">
                 {startDate} {(endDate === undefined || endDate === "") ? "" : "-" } {endDate}
             </h1>
         </Link>
