80aafe53268abdaab485d49fa5f9da30cc16317c
RileyWong26 <rileywongwong@gmail.com>
2025-07-27 20:31:03 -0400

    Fixing some errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { Anonymous_Pro } from "next/font/google";
 import "@/app/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const anonymousPro = Anonymous_Pro({
   variable: "--font-anonymous",
@@ -27,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${anonymousPro.className} antialiased`}
+        className={` ${anonymousPro.className} antialiased`}
       >
         {children}
         
