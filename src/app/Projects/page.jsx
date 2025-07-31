beb1e6442a0651111d43448376a87135b49b01be
RileyWong26 <rileywongwong@gmail.com>
2025-07-30 21:17:02 -0400

    Update to handle new backend

--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -1,13 +1,12 @@
 import Header from "@/app/header";
-import {initializeApp} from "firebase/app";
-import {getFirestore, getDocs, collection} from "firebase/firestore";
 import Image from "next/image";
 import Link from "next/link";
 import HeaderButton from "../headerbutton";
 
+
 function Tag({tech}){
     return (
-        <h1 className="text-white bg-tag px-4 py-1  rounded-2xl xl:text-xs text-xs2">
+        <h1 className="text-white bg-tag px-4 py-1 rounded-2xl text-xs">
             {tech}
         </h1>
     )
@@ -26,27 +25,27 @@ function ProjectCard({id, data}) {
             }>
             {(image === undefined || image === "") ? null : 
                 
-                <Image 
+                <Image
                     src={image}
                     className="w-full h-auto"
-                    width={0} height={0}
+                    fill
                     key={id}
                     alt="Project Image"
-                    fill
-                    unoptimized
+                    priority={true}
+                    quality={100}
+                    placeholder="empty"
                    />
             }
             
-            <div className="bg-gradient-to-b from-0% to-30% from-black/10 to-black/60 w-full h-1/4 px-4 relative
-                group-hover:h-full group-hover:to-50% translate-all duration-500 ease-in-out flex flex-col space-y-0.5 xl:space-y-1 ">
+            <section className="bg-gradient-to-b from-0% to-30% from-black/10 to-black/60 w-full h-1/4 px-4 relative 
+                group-hover:h-full group-hover:to-50% translate-all duration-500 ease-in-out flex flex-col space-y-0.5 lg:space-y-1 ">
                     
-                <div className="h-1/20 group-hover:h-1/2 transition-all duration-500 ease-in-out"/>
-                <div className="flex flex-row ">
-                    <h1 className="w-19/20 text-base xl:text-xl">
+                <div className="h-1/20 group-hover:h-1/2 transition-all duration-500 ease-in-out relative"/>
+                    <h1 className="w-19/20 text-xl truncate">
                         {id}
                     </h1>
-                    <div className="w-1/20 aspect-square -translate-x-1/2 group-hover:-translate-y-1/2 group-hover:translate-x-1/2
-                        transition-transform duration-500 ease-in-out">
+                    <div className="left-9/10 top-1/10 absolute aspect-square -translate-x-1/2 group-hover:top-1/2 group-hover:-translate-y-1/2 
+                        transition-all duration-500 ease-in-out">
                         <Image 
                             className="w-full h-auto"
                             alt="Arrow up"
@@ -54,8 +53,7 @@ function ProjectCard({id, data}) {
                             width={0} height={0}
                             quality={100}/>
                     </div>
-                </div>
-                <div className="w-full hidden xl:inline h-1/30 transition-all duration-500 ease-in-out text-transparent 
+                <div className="w-full hidden lg:inline h-1/30 transition-all duration-500 ease-in-out text-transparent 
                     xl:text-base group-hover:h-1/4 group-hover:text-alt text-ellipsis overflow-hidden">
                     { (description === undefined) ? null :
                         description
@@ -78,56 +76,73 @@ function ProjectCard({id, data}) {
                         
                     }
                 </div>
-            </div>
+                
+            </section>
         </Link>
     )
 }
 
 export default async function Projects() {
-    const firebaseConfig = {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId,
-        measurementId: process.env.measurementId,
-    }
-    // Initalize app and database
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-
-    // Fetch docs under the Projects collection
-    const results = await getDocs(collection(db, "Projects"));
+    // Fetch data from backend 
+    const sortedData = await fetch("http://backend:9000/projects", {
+        method: 'GET',
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            console.log(err); 
+            return {};
+        });
 
     // projects into project cards into an array to be mapped
     var projects = [];
-    results.forEach((item) => {
+    const keys = Object.keys(sortedData);
+    keys.map((key) => {
         projects.push(
             <ProjectCard 
-                key={item.id}
-                id={item.id}
-                data={item.data()}/>
+                key={key}
+                id={key}
+                data={sortedData[key]}/>
         );
     });
-    
 
     return (
-        <div className = "bg-background1 min-h-screen transition-colors duration-1000 ease-in-out flex flex-row space-x-2 text-primary relative">
+        <section className="bg-background1 min-h-screen  max-w-screen transition-colors 
+            duration-1000 ease-in-out flex flex-row text-primary relative">
             <Header />
             <HeaderButton />
-            <div className="w-9/10 h-full flex flex-col animate-pullup m-auto space-y-4">
-                <h1 className="text-hightlight font-bold text-2xl xl:text-3xl h-[15vh] content-end w-9/10 ml-auto">
+            <div className="w-9/10 h-full flex flex-col animate-pullup space-y-4 mx-auto">
+                <h1 className="text-hightlight font-bold text-3xl h-[15vh] content-end w-9/10 ml-auto">
                     Projects
                 </h1>
-                <div className="min-h-full w-full grid grid-cols-2 xl:grid-cols-4 gap-4 mb-4">
+                <div className="w-full xl:w-9/10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-auto">
                     {projects.map((item) => {
                         return(
                             item
                         );
                     })}
                 </div>
+                <Link className="lg:pl-34 pl-14 text-primary w-8/9 lg:w-1/2 mt-4 flex flex-row text-xl space-x-4 content-center h-6 "
+                    href="/Contact"> 
+                    <h1 className="content-center">
+                        CONTACT
+                    </h1>
+                    <Image 
+                        className="my-auto h-3/5 w-auto hidden dark:inline animate-bounceright"
+                        width={1000} height={1000}
+                        alt="Dark Mode Right Arrow"
+                        src={"/DarkMode/RightArrow.webp"}/>
+                        <Image 
+                        className="my-auto h-3/5 w-auto dark:hidden animate-bounceright"
+                        width={1000} height={1000}
+                        alt="Light Mode Right Arrow"
+                        src={"/LightMode/RightArrow.webp"}/>
+                </Link>
             </div>
-        </div>  
+        </section>  
     )
 }
