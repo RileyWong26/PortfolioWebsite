bdaf47f27f3b81a6d9652b840354e37cb6f6d861
RileyWong26 <rileywongwong@gmail.com>
2025-09-11 23:14:50 -0400

    Fix too many h1 seo

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -10,15 +10,17 @@ export default async function About(){
             <HeaderButton />
             <div className="w-full flex flex-col animate-pullup min-h-screen ">
                 {/*Page Title */}
-                <h1 className="text-hightlight h-[15vh] font-bold text-3xl w-4/5 mx-auto content-end ">About Me</h1>
+                <h1 className="text-hightlight h-[15vh] font-bold text-3xl w-4/5 mx-auto content-end ">
+                    About Me
+                </h1>
                 {/*Page content */}
                 <section className="w-full flex flex-col-reverse lg:flex-row py-10 space-y-5 lg:h-[80vh] content-center ">
                     {/* Left side text*/}
                     <div className="mx-auto w-full lg:min-h-[50vh] lg:h-full ">
                         <div className="lg:ml-auto lg:mr-0 m-auto w-4/5 h-full flex flex-col space-y-8 lg:space-y-12 justify-center">
-                            <h1 className="text-base leading-6">Hi! My name is Riley Wong, I am a developer passionate about creating robust software.  My favourite works lies in creating software that solves issues and facilitates use for its users.  My love for developing started from creating both mods and games at age 10.  </h1>
-                            <h1 className="text-base leading-6">I am a team player who communicates effectively, faces problems critically and has the technical skills to tackle any problems.  </h1>
-                            <h1 className="text-base leading-6">In my spare time I like to listen to music, play guitar and sing.</h1>
+                            <h2 className="text-base leading-6">Hi! My name is Riley Wong, I am a developer passionate about creating robust software.  My favourite works lies in creating software that solves issues and facilitates use for its users.  My love for developing started from creating both mods and games at age 10.  </h2>
+                            <h2 className="text-base leading-6">I am a team player who communicates effectively, faces problems critically and has the technical skills to tackle any problems.  </h2>
+                            <h2 className="text-base leading-6">In my spare time I like to listen to music, play guitar and sing.</h2>
                         </div>
                     </div>
                     {/*Right side Image and description */}
@@ -33,15 +35,15 @@ export default async function About(){
                                     quality={100}
                                     alt="AboutMeImage"/>
                             </div>
-                            <h1 className=" w-3/4 m-auto text-center mt-5 text-sm"> Picture of me,(left most), and my friends winning the 2025 LoJam X GDS Game Jam</h1>
+                            <h2 className=" w-3/4 m-auto text-center mt-5 text-sm"> Picture of me,(left most), and my friends winning the 2025 LoJam X GDS Game Jam</h2>
                         </div>
                     </div>
                 </section>
                 <Link className="lg:pl-34 pl-14 text-primary w-8/9 lg:w-1/2 lg:-translate-y-20 flex flex-row text-xl space-x-4 content-center h-6"
                     href="/Experience"> 
-                    <h1 className="content-center">
+                    <h2 className="content-center">
                         EXPERIENCE
-                    </h1>
+                    </h2>
                     <Image 
                         className="my-auto h-3/5 w-auto hidden dark:inline animate-bounceright"
                         width={1000} height={1000}
