1c9ebb69271e1180e3102ac387603dd3dd87c142
RileyWong26 <rileywongwong@gmail.com>
2025-09-16 00:26:00 -0400

    Focus on certain experience cards, and fixing issues with text overflow

--- src/app/Experience/ExperienceCard.jsx
+++ src/app/Experience/ExperienceCard.jsx
@@ -8,9 +8,29 @@ export default function ExperienceCard({title, data}){
     const startDate = data.StartDate;
     const endDate = data.EndDate;
 
+    function handleFocus() {
+        const cards = document.getElementsByClassName("experience-card");
+        for(let i = 0; i<cards.length; i++){
+            const curr = cards[i];
+            if(curr.id !== `Experience ${title}`){
+                curr.classList.add("opacity-65")
+            }
+        }
+    }
+    function removeFocus(){
+        const cards = document.getElementsByClassName("experience-card");
+        for(let i = 0; i<cards.length; i++){
+            const curr = cards[i];
+            if(curr.id !== `Experience ${title}`){
+                curr.classList.remove("opacity-65")
+            }
+        }
+    }
+
     return (
-        <Link className="w-full h-[33vh] hover:bg-hightlight/20 rounded-2xl p-5 flex flex-col group space-y-2 
-            transition-all duration-300 ease-in-out" 
+        <Link className="w-full h-[33vh] hover:bg-linear-to-b from-background1 to-hightlight/20 rounded-2xl p-5 flex flex-col group space-y-2 
+            transition-all duration-300 ease-in-out experience-card" 
+            id={`Experience ${title}`}
             href={{
                 pathname:`/Experience/${title}`,
             }}
@@ -19,41 +39,46 @@ export default function ExperienceCard({title, data}){
                 const doc = document.getElementById(title);
                 (doc === null) ? null : doc.classList.toggle("animate-pullup2");
                 document.getElementById("ExperienceImageContainer").classList.toggle("animate-turn");
+                handleFocus();
             }}
             onMouseLeave={()=> {
                 const doc = document.getElementById(title);
                 (doc === null) ? null : doc.classList.toggle("animate-pullup2");
                 document.getElementById("ExperienceImageContainer").classList.toggle("animate-turn");
+                removeFocus();
             }}>
             <div className="w-full flex flex-row-reverse group relative">
                 <div className="absolute left-9/10  aspect-square 
                     group-hover:translate-x-1/2 group-hover:-translate-y-1/6
                     transition-transform duration-500 ease-in-out">
                 
-                    <Image className="w-auto h-full lg:w-3/5 lg:h-auto hidden dark:block"
+                    <Image className="w-auto h-full md:w-3/5 md:h-auto hidden dark:block"
                         src="/DarkMode/ArrowUpDarkMode.webp"
                         width={40} height={40}
                         quality={100}
                         alt=""/>
-                    <Image className="w-auto h-full lg:w-3/5 lg:h-auto dark:hidden"
+                    <Image className="w-auto h-full md:w-3/5 md:h-auto dark:hidden"
                         src="/LightMode/ArrowUpLightMode.webp"
                         width={40} height={40}
                         quality={100}
                         alt=""/>
                 </div>
-                <h1 className="w-full content-center text-xl ">
+                <h2 className="w-full content-center text-xl ">
                     {title}
-                </h1>
+                </h2>
             </div>
-            <h1 className="text-text2 text-lg ">
+            <h2 className="text-text2 text-lg ">
                 {role}
-            </h1>
-            <h1 className="text-base h-5/10 text-wrap truncate">
-                {description}
-            </h1>
-            <h1 className="text-right h-1/10 text-base text-text2 text-balanced">
+            </h2>
+            <div className="text-base line-clamp-5">
+                <h2 id="description" 
+                    className="">
+                    {description}
+                </h2>
+            </div>
+            <h2 className="text-right mt-auto text-base text-text2 text-balanced">
                 {startDate} {(endDate === undefined || endDate === "") ? "" : "-" } {endDate}
-            </h1>
+            </h2>
         </Link>
     )
 }
