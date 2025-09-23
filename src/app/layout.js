9212a851a43e51b31367e8670392e21530c5a9dc
RileyWong26 <rileywongwong@gmail.com>
2025-09-22 22:39:43 -0400

    Fixed an error

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,9 +15,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en" suppressHydrationWarning >
       <body
-        className={` ${anonymousPro.className} antialiased`} suppressHydrationWarning >
+        className={` ${anonymousPro.className} antialiased`} suppressHydrationWarning>
           <Providers>
             {children}
           </Providers>
